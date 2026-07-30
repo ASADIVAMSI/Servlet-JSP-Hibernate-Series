c09dc7790827ac075c571c0e66dc3caedc6a83d3
ASADI VAMSI <165474708+ASADIVAMSI@users.noreply.github.com>
2026-07-30 23:41:07 +0530

    Add files via upload

---
+++ java-webapp-17042026-practice/src/main/java/servlet/EmployeesListServlet.java
@@ -0,0 +1,89 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/employeeslist")
+public class EmployeesListServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+
+		out.println("<html>");
+		out.println("<body bgcolor='yellow'>");
+		
+		out.println("<table border='10' width='100%'>");
+		out.println("<tr>");
+		out.println("<th>Employee ID</th>");
+		out.println("<th>Name</th>");
+		out.println("<th>Salary</th>");
+		out.println("<th>Address</th>");
+		out.println("<th>Date of Joining</th>");
+		out.println("<th>Age</th>");
+		out.println("</tr>");
+		
+		Connection dbConn = null;
+		Statement dbStmt = null;
+		ResultSet dbRs = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			dbConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jfs49", "root", "root");
+			dbStmt = dbConn.createStatement();
+
+			String sqlQuery = "SELECT * FROM employee";
+			dbRs = dbStmt.executeQuery(sqlQuery);
+
+			while (dbRs.next()) {
+				int empId = dbRs.getInt(1);
+				String name = dbRs.getString(2);
+				double salary = dbRs.getDouble(3);
+				
+				out.println("<tr>");
+				out.println("<td>" + empId + "</td>");
+				out.println("<td>" + name + "</td>");
+				out.println("<td>" + salary + "</td>");
+				out.println("<td>" + dbRs.getString(4) + "</td>");
+				out.println("<td>" + dbRs.getString(5) + "</td>");
+				out.println("<td>" + dbRs.getString(6) + "</td>");
+				out.println("</tr>");
+			}
+		} catch (ClassNotFoundException cnfEx) {
+			cnfEx.printStackTrace();
+		} catch (SQLException sqlEx) {
+			sqlEx.printStackTrace();
+		} finally {
+
+			try {
+				if (dbStmt != null) {
+					dbStmt.close();
+
+				}
+				if (dbConn != null) {
+					dbConn.close();
+
+				}
+
+			} catch (SQLException sqlEx) {
+				sqlEx.printStackTrace();
+			}
+		}
+		
+		out.println("</table>");
+		
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
